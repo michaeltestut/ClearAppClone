c649deb57341067b91678852b2c63a96878864fb
Michael Testut <michael.testut@yahoo.com>
2023-05-25 21:39:06 -0700

    start CategoryViewController

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
-
+    
+    var categoryArray : [ItemCategory] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController!.view.backgroundColor = .systemBlue
+        
+        //loadCategories()
     }
+    
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        
+        return cell
+    
+    }
+    //MARK: - TableView Delegate Methods
 
+    //MARK: - Add New Category
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     }
+    
+    
+    //MARK: - Data Manipulation Methods
 }
